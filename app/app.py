5198731fdd99f5095b980d878150a1fdc93b0e3d
David salamanca <d.salamancaz@uniandes.edu.co>
2023-10-29 19:36:12 -0500

    Test

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
 from celery_config import celery_init_app
-from vistas import VistaSignUp, VistaLogin, VistaTasks, VistaStatus, DocumentDownloadIn, DocumentDownloadOut
+from vistas import VistaSignUp, VistaLogin ,VistaStatus #, DocumentDownloadIn, DocumentDownloadOut, VistaTasks
 
 
 app = Flask(__name__)
@@ -28,10 +28,10 @@ cors = CORS(app)
 api = Api(app)
 api.add_resource(VistaSignUp, '/api/auth/signup')
 api.add_resource(VistaLogin, '/api/auth/login')
-api.add_resource(VistaTasks, '/api/tasks', '/api/tasks/<int:id_task>')
+# api.add_resource(VistaTasks, '/api/tasks', '/api/tasks/<int:id_task>')
 api.add_resource(VistaStatus, '/status')
-api.add_resource(DocumentDownloadIn, '/api/tasks/<int:id_task>/downloadin')
-api.add_resource(DocumentDownloadOut, '/api/tasks/<int:id_task>/downloadout')
+# api.add_resource(DocumentDownloadIn, '/api/tasks/<int:id_task>/downloadin')
+# api.add_resource(DocumentDownloadOut, '/api/tasks/<int:id_task>/downloadout')
 
 jwt = JWTManager(app)
 
