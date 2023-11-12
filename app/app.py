c4b08e5838635e6401da972ff51d69c8f209b9b5
David salamanca <d.salamancaz@uniandes.edu.co>
2023-11-12 10:53:08 -0500

    Archivo de variables

--- app/app.py
+++ app/app.py
@@ -8,16 +8,24 @@ from modelos import db
 from celery_config import celery_init_app
 from vistas import VistaSignUp, VistaLogin, VistaTasks, VistaStatus, DocumentDownloadIn, DocumentDownloadOut
 
-from ..variables import IpPostgres, IpRedis
+import json
+from os.path import abspath, dirname, join
+
+config_file_path = abspath(join(dirname(__file__), '..', 'config.json'))
+
+with open(config_file_path, 'r') as config_file:
+    config_data = json.load(config_file)
+
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://postgres:postgres@{IpPostgres}:5432/postgres"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://postgres:postgres@{config_data['IpPostgres']}:5432/postgres"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'MISO-Nube'
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config['CELERY_CONFIG'] = {
-    "broker_url": f'redis://{IpRedis}:6379/0',
-    "result_backend": f'redis://{IpRedis}:6379/0',
+    "broker_url": f'redis://{config_data['IpRedis']}:6379/0',
+    "result_backend": f'redis://{config_data['IpRedis']}:6379/0',
 }
 
 # Initialize the application context
