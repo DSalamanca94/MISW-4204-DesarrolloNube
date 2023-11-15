8728dc606bc94d2b4cd466452cc4335ce956df77
David salamanca <d.salamancaz@uniandes.edu.co>
2023-11-15 18:53:35 -0500

    Ajustes servicio descargar Files

--- app/vistas/vistas.py
+++ app/vistas/vistas.py
@@ -169,42 +169,42 @@ class VistaTasks(Resource):
         return {"mensaje": "Documento eliminado"}, 204
 
 # Estas vistas fueron creadas para descargar el archivo de entrada y el archivo de salida
-    class DocumentDownloadOut(Resource):
-        def get(self, id_task):
-            document = Document.query.get(id_task)
-            if document is None:
-                return {"error": "Documento no encontrado"}, 404
+class DocumentDownloadOut(Resource):
+    def get(self, id_task):
+        document = Document.query.get(id_task)
+        if document is None:
+            return {"error": "Documento no encontrado"}, 404
 
-            storage_client = storage.Client()
-            bucket = storage_client.get_bucket('app-storage-folder')
-            blob = bucket.blob(f"Output/{document.id}.{document.format_out.value}")
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket('app-storage-folder')
+        blob = bucket.blob(f"Output/{document.id}.{document.format_out.value}")
 
-            file_content = BytesIO()
-            blob.download_to_file(file_content)
-            file_content.seek(0)
+        file_content = BytesIO()
+        blob.download_to_file(file_content)
+        file_content.seek(0)
 
-            return send_file(
-                file_content,
-                as_attachment=True,
-                attachment_filename=f"{document.id}.{document.format_out.value}"
-            )
+        return send_file(
+            file_content,
+            as_attachment=True,
+            attachment_filename=f"{document.id}.{document.format_out.value}"
+        )
 
-    class DocumentDownloadIn(Resource):
-        def get(self, id_task):
-            document = Document.query.get(id_task)
-            if document is None:
-                return {"error": "Documento no encontrado"}, 404
+class DocumentDownloadIn(Resource):
+    def get(self, id_task):
+        document = Document.query.get(id_task)
+        if document is None:
+            return {"error": "Documento no encontrado"}, 404
 
-            storage_client = storage.Client()
-            bucket = storage_client.get_bucket('app-storage-folder')
-            blob = bucket.blob(f"Input/{document.id}.{document.format_in.value}")
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket('app-storage-folder')
+        blob = bucket.blob(f"Input/{document.id}.{document.format_in.value}")
 
-            file_content = BytesIO()
-            blob.download_to_file(file_content)
-            file_content.seek(0)
+        file_content = BytesIO()
+        blob.download_to_file(file_content)
+        file_content.seek(0)
 
-            return send_file(
-                file_content,
-                as_attachment=True,
-                attachment_filename=f"{document.id}.{document.format_in.value}"
-            )
+        return send_file(
+            file_content,
+            as_attachment=True,
+            attachment_filename=f"{document.id}.{document.format_in.value}"
+        )
